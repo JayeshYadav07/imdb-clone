2af29f15698f9b54b06e77486e8ee427999de14c
JayeshYadav57 <jayeshyadav8080@gmail.com>
2023-08-09 12:42:03 +0530

    styling added

--- src/App.js
+++ src/App.js
@@ -2,6 +2,8 @@ import "./App.css";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Home from "./pages/home/Home";
 import Header from "./components/header/Header";
+import MovieList from "./components/movieList/MovieList";
+import MovieDetail from "./pages/movieDetail/MovieDetail";
 function App() {
     return (
         <div className="App">
@@ -9,14 +11,8 @@ function App() {
                 <Header></Header>
                 <Routes>
                     <Route index element={<Home />} />
-                    <Route
-                        path="movie/:id"
-                        element={<h1>movie details page</h1>}
-                    />
-                    <Route
-                        path="movies/:type"
-                        element={<h1>movies type page</h1>}
-                    />
+                    <Route path="movie/:id" element={<MovieDetail />} />
+                    <Route path="movies/:type" element={<MovieList />} />
                     <Route path="/*" element={<h1>Error : 404 Not Found</h1>} />
                 </Routes>
             </BrowserRouter>
--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -1,16 +1,67 @@
-import React from "react";
-import { useEffect, useState } from "react";
-function Home() {
-    const [date, setDate] = useState([]);
+import React, { useEffect, useState } from "react";
+import "./Home.css";
+import "react-responsive-carousel/lib/styles/carousel.min.css";
+import { Carousel } from "react-responsive-carousel";
+import { Link } from "react-router-dom";
+import MovieList from "../../components/movieList/MovieList";
+
+const Home = () => {
+    const [popularMovies, setPopularMovies] = useState([]);
+
     useEffect(() => {
         fetch(
             "https://api.themoviedb.org/3/movie/popular?api_key=339061d81d640d76bc091664f6a2aab9&language=en-US"
         )
             .then((res) => res.json())
-            .then((result) => console.log(result));
+            .then((data) => setPopularMovies(data.results));
     }, []);
 
-    return <div>Home</div>;
-}
+    return (
+        <>
+            <div className="poster">
+                <Carousel
+                    showThumbs={false}
+                    autoPlay={true}
+                    transitionTime={3}
+                    infiniteLoop={true}
+                    showStatus={false}
+                >
+                    {popularMovies.map((movie) => (
+                        <Link
+                            key={movie.id}
+                            style={{ textDecoration: "none", color: "white" }}
+                            to={`/movie/${movie.id}`}
+                        >
+                            <div className="posterImage">
+                                <img
+                                    src={`https://image.tmdb.org/t/p/original${
+                                        movie && movie.backdrop_path
+                                    }`}
+                                    alt="error"
+                                />
+                            </div>
+                            <div className="posterImage__overlay">
+                                <div className="posterImage__title">
+                                    {movie ? movie.original_title : ""}
+                                </div>
+                                <div className="posterImage__runtime">
+                                    {movie ? movie.release_date : ""}
+                                    <span className="posterImage__rating">
+                                        {movie ? movie.vote_average : ""}
+                                        <i className="fas fa-star" />{" "}
+                                    </span>
+                                </div>
+                                <div className="posterImage__description">
+                                    {movie ? movie.overview : ""}
+                                </div>
+                            </div>
+                        </Link>
+                    ))}
+                </Carousel>
+                <MovieList />
+            </div>
+        </>
+    );
+};
 
 export default Home;
